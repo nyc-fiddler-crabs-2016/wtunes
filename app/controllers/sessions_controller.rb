68368eae90185367b1a49f4f0ef63fdb22004b27
will van wart <william@wvwproductions.com>
2016-03-24 10:18:41 -0400

    add sessions_controller/view/route

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by(user_name: params[:user_name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] =  "#{@user.user_name} is successfully logged in."
+      redirect_to posts_path
+    else
+      flash[:error] = "username or password is incorrect."
+      render 'new'
+  end
+end
+
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path
+  end
+
+
+end
